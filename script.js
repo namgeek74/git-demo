e5c2c4d9826513fcf509122cb0d0b93729be778d
namnd <dinhnambk96@gmail.com>
2021-07-10 18:15:23 +0700

    test es 2021

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let a = 1;
+// let a = 1;
+// let b=  5;
+// a && (b = a);
+// console.log(b);
 
-function test() {
-  console.log("something");
-}
+let a = 100_000;
+console.log(a);
+
+let str = "Nam, what are you doing, i think you should be change Nam to Huy";
+let newStr = str.replaceAll("Nam", "Khanh");
+console.log(newStr);
